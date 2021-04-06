14fe0e2738e82e7a56ebc8f05be320bc721f83cc
Dejan85 <xypnotica@gmail.com>
2021-04-07 01:00:03 +0200

    started creating navigation on frontend

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 const root = document.getElementById("root");
 
-ReactDOM.render(<App />, root);
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  root
+);

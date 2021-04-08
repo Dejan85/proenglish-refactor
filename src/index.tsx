5732bfce4ca52097d5a88395f90b210150c18077
Dejan85 <xypnotica@gmail.com>
2021-04-08 21:41:50 +0200

    conected redux to components

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./components/styles/main.scss";
+import { Provider } from "react-redux";
+import { ConnectedRouter } from "connected-react-router";
+import history from "./utils/history";
+import configureStore from "./redux/configureStore";
+
+const initialState = {};
+const store = configureStore(initialState, history);
 
 const root = document.getElementById("root");
 
 ReactDOM.render(
-  <App />,
-
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>,
   root
 );

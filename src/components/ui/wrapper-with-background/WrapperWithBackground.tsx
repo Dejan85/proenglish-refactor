9d0d90f5916711ded7d94c536d9e0f709ebda26c
Dejan85 <xypnotica@gmail.com>
2021-04-09 04:50:22 +0200

    fixed types

--- src/components/ui/wrapper-with-background/WrapperWithBackground.tsx
+++ src/components/ui/wrapper-with-background/WrapperWithBackground.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import { WrapperType } from "~/src/global-types";
 import { WrapperWithBackgroundProps } from "./types";
 
-const WrapperWithBackground = (props: WrapperWithBackgroundProps) => {
-  const Wrapper: WrapperType = "div";
+const Wrapper: WrapperType = "div";
+const className: string = "wrapper-with-background";
+
+const WrapperWithBackground = (
+  props: WrapperWithBackgroundProps
+): JSX.Element => {
   const { children } = props;
-  return <Wrapper className="wrapper-with-background">{children}</Wrapper>;
+  return <Wrapper className={className}>{children}</Wrapper>;
 };
 
 export default WrapperWithBackground;

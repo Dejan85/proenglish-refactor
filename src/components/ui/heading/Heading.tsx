38853852264c0bc9d5630720512f9624c7d89e0a
Dejan85 <xypnotica@gmail.com>
2021-04-09 07:02:58 +0200

    added className prop to heading

--- src/components/ui/heading/Heading.tsx
+++ src/components/ui/heading/Heading.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import { HeadingProps } from "./types";
 
 function Heading(props: HeadingProps): JSX.Element {
-  const { as = "h1", text } = props;
+  const { as = "h1", text, className: customClassName } = props;
   const Wrapper = as;
-  const className: string = `heading__${as}`;
+  const className: string = `heading__${customClassName || as}`;
   return <Wrapper className={className}>{text}</Wrapper>;
 }
 

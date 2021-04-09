c60a16ce57821f654263201dfd0bcefd9f004aaf
Dejan85 <xypnotica@gmail.com>
2021-04-09 06:38:51 +0200

    created content container component

--- src/components/frontend/home/pages/about/About.tsx
+++ src/components/frontend/home/pages/about/About.tsx
@@ -1,15 +1,31 @@
 import React from "react";
 import { WrapperType } from "~/src/global-types";
-import { ContentContainer, Text, Heading } from "~/src/components/ui";
+import {
+  ContentContainer,
+  SubContainer,
+  Text,
+  Heading,
+} from "~/src/components/ui";
+import { aboutText } from "./messages";
+import uniqid from "uniqid";
 
 const Wrapper: WrapperType = "div";
 const className: string = "about";
 
+const generateText = (): JSX.Element[] =>
+  aboutText.map((text) => (
+    <Text key={uniqid()} as="p" text={text} className="about" />
+  ));
+
+const text = generateText();
+
 const About = () => {
   return (
-    <Wrapper className={className}>
-      <ContentContainer></ContentContainer>
-    </Wrapper>
+    <SubContainer>
+      <Wrapper className={className}>
+        <ContentContainer>{text}</ContentContainer>
+      </Wrapper>
+    </SubContainer>
   );
 };
 
--- src/components/ui/content-container/ContentContainer.tsx
+++ src/components/ui/content-container/ContentContainer.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { WrapperType } from "~/src/global-types";
 import { ContentContainerProps } from "./types";
+
 const Wrapper: WrapperType = "div";
+const className: string = "content-container";
 
 const ContentContainer = (props: ContentContainerProps): JSX.Element => {
   const { children } = props;
-  return <Wrapper>{children}</Wrapper>;
+  return <Wrapper className={className}>{children}</Wrapper>;
 };
 
 export default ContentContainer;

ee10b301a44ab2fae31c9ede2bd342273187aed3
Dejan85 <xypnotica@gmail.com>
2021-04-09 04:36:33 +0200

    fixed types

--- src/components/ui/container/Container.tsx
+++ src/components/ui/container/Container.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import { ContainerProps } from "./types";
 import { WrapperType } from "~/src/global-types";
 
+const Wrapper: WrapperType = "div";
+
 const Container = (props: ContainerProps): JSX.Element => {
   const { children } = props;
-  const Wrapper: WrapperType = "div";
   return <Wrapper className="container">{children}</Wrapper>;
 };
 

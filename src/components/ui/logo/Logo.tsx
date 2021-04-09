8d3298889e2ec45989c0c3c8c0236656420871fe
Dejan85 <xypnotica@gmail.com>
2021-04-09 04:40:39 +0200

    fixed types

--- src/components/ui/logo/Logo.tsx
+++ src/components/ui/logo/Logo.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { WrapperType } from "~/src/global-types";
 import img from "./images/logo2.png";
 
+const Wrapper: WrapperType = "div";
+const className: string = "logo";
+
 const Logo = (): JSX.Element => {
-  const Wrapper: WrapperType = "div";
   return (
-    <Wrapper className="logo">
+    <Wrapper className={className}>
       <img src={img} alt="img" />
     </Wrapper>
   );

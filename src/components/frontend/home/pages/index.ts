2f0cec6e47bcec874db0a9cced4b908ad600c917
Dejan85 <xypnotica@gmail.com>
2021-04-10 04:19:43 +0200

    testing lazyload

--- src/components/frontend/home/pages/index.ts
+++ src/components/frontend/home/pages/index.ts
@@ -1,5 +1,14 @@
 import loadable from '~/src/utils/loadable';
+// import loadableVisibility from "react-loadable-visibility/loadable-components";
 
 export const WelcomePage: (props: any) => JSX.Element = loadable(() => import('./welcome-page/WelcomePage'));
 export const About: (props: any) => JSX.Element = loadable(() => import('./about/About'));
 export const Course: (props: any) => JSX.Element = loadable(() => import('./course/Course'));
+
+
+
+
+// export const WelcomePage = loadableVisibility(() => import('./welcome-page/WelcomePage'));
+// export const About = loadableVisibility(() => import('./about/About'));
+// export const Course = loadableVisibility(() => import("./course/Course"));
+

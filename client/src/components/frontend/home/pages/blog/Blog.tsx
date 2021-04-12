97d0b09bac56aa35c33608d0c291821cc183a2d1
Dejan85 <xypnotica@gmail.com>
2021-04-12 11:13:57 +0200

    refactor blog

--- client/src/components/frontend/home/pages/blog/Blog.tsx
+++ client/src/components/frontend/home/pages/blog/Blog.tsx
@@ -15,13 +15,8 @@ import {
 } from "~/src/components/ui";
 import { headingText } from "./messages";
 
-const renderHeading = (): JSX.Element => (
-  <Heading as="h1" text={headingText} className="default-heading" />
-);
-
 const renderLine = (): JSX.Element => <Line />;
 
-const headingUi = renderHeading();
 const lineUi = renderLine();
 
 const Blog = (): JSX.Element => {
@@ -38,7 +33,9 @@ const Blog = (): JSX.Element => {
   return (
     <SubContainer>
       <ContentContainer className="justify-content-center flex-direction-column">
-        {headingUi}
+        <Heading as="h1" className="default-heading">
+          {headingText}
+        </Heading>
         {lineUi}
       </ContentContainer>
     </SubContainer>

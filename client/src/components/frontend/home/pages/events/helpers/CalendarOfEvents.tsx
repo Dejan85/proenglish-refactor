167f7018bf75c94dc8cd04d34f8083116e510981
Dejan85 <xypnotica@gmail.com>
2021-04-18 20:54:28 +0200

    added created CalendarOfEvents component

---
+++ client/src/components/frontend/home/pages/events/helpers/CalendarOfEvents.tsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import { ContentContainer, Heading, CalendarUi } from "~/src/components/ui";
+import { filterEventsForCurrentMonth } from "~/src/utils/timeAndDateHandlers";
+import { UiRenderTypes } from "../types";
+
+const CalendarOfEvents = (props: UiRenderTypes): JSX.Element => {
+  const { eventsData } = props;
+  const [highlightDates, setHighlightDates] = useState<Date[]>();
+
+  const generateHighlhtDates = (value = new Date()) => {
+    const filteredEvents = filterEventsForCurrentMonth(eventsData, value);
+    const fullYear = value.getFullYear();
+    const month = value.getMonth();
+    const highlightDates = filteredEvents.map((item) => {
+      const { date } = item;
+      return new Date(Number(fullYear), month, Number(date.slice(0, 2)));
+    });
+
+    setHighlightDates(highlightDates);
+  };
+
+  useEffect(() => {
+    generateHighlhtDates();
+  }, [eventsData]);
+
+  return (
+    <ContentContainer
+      maxWidth="39rem"
+      height="57rem"
+      marginTop="10rem"
+      flexDirection="column"
+      justifyContent="space-between"
+      position="relative"
+    >
+      <Heading className="h2" as="h2">
+        Kalendar događaja
+      </Heading>
+
+      <ContentContainer position="absolute" top="10rem">
+        <CalendarUi
+          highlightDates={highlightDates}
+          generateHighlhtDates={generateHighlhtDates}
+        />
+      </ContentContainer>
+    </ContentContainer>
+  );
+};
+
+export default CalendarOfEvents;
--- client/src/components/frontend/home/pages/events/helpers/index.tsx
+++ client/src/components/frontend/home/pages/events/helpers/index.tsx
@@ -1,57 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import {
   SubContainer,
   ContentContainer,
   Heading,
-  CalendarUi,
   CalendarInfo,
 } from "~/src/components/ui";
 import { Scrollbars } from "react-custom-scrollbars";
-import { filterEventsForCurrentMonth } from "~/src/utils/timeAndDateHandlers";
-import { UiRenderTypes } from "../types";
-
-const CalendarOfEvents = (props: UiRenderTypes): JSX.Element => {
-  const { eventsData } = props;
-  const [highlightDates, setHighlightDates] = useState<Date[]>();
-
-  const generateHighlhtDates = (value = new Date()) => {
-    const filteredEvents = filterEventsForCurrentMonth(eventsData, value);
-    const fullYear = value.getFullYear();
-    const month = value.getMonth();
-    const highlightDates = filteredEvents.map((item) => {
-      const { date } = item;
-      return new Date(Number(fullYear), month, Number(date.slice(0, 2)));
-    });
-
-    setHighlightDates(highlightDates);
-  };
-
-  useEffect(() => {
-    generateHighlhtDates();
-  }, [eventsData]);
-
-  return (
-    <ContentContainer
-      maxWidth="39rem"
-      height="57rem"
-      marginTop="10rem"
-      flexDirection="column"
-      justifyContent="space-between"
-      position="relative"
-    >
-      <Heading className="h2" as="h2">
-        Kalendar događaja
-      </Heading>
-
-      <ContentContainer position="absolute" top="10rem">
-        <CalendarUi
-          highlightDates={highlightDates}
-          generateHighlhtDates={generateHighlhtDates}
-        />
-      </ContentContainer>
-    </ContentContainer>
-  );
-};
+import CalendarOfEvents from "./CalendarOfEvents";
 
 const MonthlyEvents = (): JSX.Element => {
   return (

288a79b7c6a33fb1dc34c41121bce1ec662e77b4
Dejan85 <xypnotica@gmail.com>
2021-04-20 20:07:47 +0200

    created selectors

--- client/src/components/frontend/about/About.tsx
+++ client/src/components/frontend/about/About.tsx
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { ContentContainer, SubContainer } from "~/src/components/ui";
 import "./styles.scss";
 import background from "./images/background.jpg";
+import { useInjectSaga } from "~/src/utils/injectSaga";
+import { useInjectReducer } from "~/src/utils/injectReducer";
+import { useDispatch, useSelector } from "react-redux";
+import { getAboutData } from "./selectors";
+import { ABOUT_SCOPE } from "./constants";
+import { fetchAboutDataAction, reducer } from "./slice";
+import saga from "./saga";
 
 const About = () => {
+  useInjectReducer({ key: ABOUT_SCOPE, reducer });
+  useInjectSaga({ key: ABOUT_SCOPE, saga });
+  const dispatch = useDispatch();
+  const { aboutData } = useSelector(getAboutData);
+
+  useEffect(() => {
+    dispatch(fetchAboutDataAction());
+  }, []);
+
+  console.log("test", aboutData);
+
   return (
     <ContentContainer className="about">
       <ContentContainer
         className="about__background"
         backgroundImage={background}
       />
-      <SubContainer></SubContainer>
+      <SubContainer>x</SubContainer>
     </ContentContainer>
   );
 };

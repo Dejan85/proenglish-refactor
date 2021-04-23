add7388605af18264e02a9c84c80648ee4affefe
Dejan85 <xypnotica@gmail.com>
2021-04-23 11:28:59 +0200

    blog modal page done

--- client/src/components/frontend/blog/partials/RenderUi.tsx
+++ client/src/components/frontend/blog/partials/RenderUi.tsx
@@ -4,15 +4,54 @@ import {
   SubContainer,
   Heading,
   Line,
+  Image,
+  Text,
+  Link,
+  Loading,
 } from "~/src/components/ui";
 import { RenderUiProps } from "../types";
+import uniqid from "uniqid";
 
-const BlogModal = () => {};
+const BlogModal = (props: {
+  blog:
+    | { photo: string; created: Date; title: string; description: string }
+    | any;
+}): JSX.Element => {
+  const { blog } = props;
+  console.log("test", blog);
+
+  return (
+    <ContentContainer className="blog__modal-container">
+      <ContentContainer className="blog__modal-image-container">
+        <Image
+          alt="img"
+          src={blog.photo}
+          type="base64"
+          className="blog__modal-image"
+        />
+      </ContentContainer>
+      <ContentContainer className="blog__modal-content">
+        <Text className="blog__modal-date" as="span">
+          {blog.created}
+        </Text>
+        <Heading className="blog__modal-heading" as="h1">
+          {blog.title}
+        </Heading>
+        <Text className="blog__modal-text" as="p">
+          {blog.description.substr(0, 400)}...
+        </Text>
+        <ContentContainer>
+          <Link className="blog__modal-link" as="a" href="/">
+            Procitaj sve
+          </Link>
+        </ContentContainer>
+      </ContentContainer>
+    </ContentContainer>
+  );
+};
 
 const RenderUi = (props: RenderUiProps): JSX.Element => {
   const { blogData } = props;
-  console.log("test", blogData);
-
   return (
     <ContentContainer className="blog">
       <SubContainer className="blog__sub-container">
@@ -23,6 +62,15 @@ const RenderUi = (props: RenderUiProps): JSX.Element => {
           <Line.SmallLine />
           <Line.LargeLine />
         </Line>
+        <>
+          {blogData.length ? (
+            blogData.map(
+              (blog): JSX.Element => <BlogModal key={uniqid()} blog={blog} />
+            )
+          ) : (
+            <Loading height="50vh" />
+          )}
+        </>
       </SubContainer>
     </ContentContainer>
   );
